91c872e170c6f633ce38f329615f678f582a252b
unknown <grosheff.ivan@gmail.com>
2022-09-22 12:38:02 +0300

    first commit

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import "./modules/burger.js";
-import "./modules/swiper.js";
-import "./modules/typingText.js";
-import "./modules/accordion.js";
-import "./modules/popup.js";
-import "./modules/ScrollTo.js";
-import "./modules/date.js";
--- src/js/modules/accordion.js
+++
@@ -1,8 +0,0 @@
-const faqs = document.querySelectorAll(".manual__content__accordion")
-
-faqs.forEach((faq) => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("active");
-    });
-});
-
